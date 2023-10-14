ee22aa59a79de328ffcfc6ce7afa16b14fb3a0a6
Vishal Yadav <vishalyxo@gmail.com>
2023-10-14 16:53:21 +0530

    First Commit

---
+++ src/conf/conf.js
@@ -0,0 +1,10 @@
+const conf = {
+    appwriteUrl: "https://cloud.appwrite.io/v1",
+    appwriteProjectId: "6526d0756291bd9f951c",
+    appwriteDatabaseId: "6526d17048c8cc104886",
+    appwriteCollectionId: "6526d1ad0728aeef49f6",
+    appwriteBucketId: "6526d43db2f458a35b8e",
+}
+// there was a name issue with the import.meta.env.VITE_APPWRITE_URL, it was later fixed in debugging video
+// appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+export default conf
